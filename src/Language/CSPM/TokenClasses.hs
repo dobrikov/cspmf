7f8a89fbef2ed1e21b74d79346ce5bb24328ce42
Marc Fontaine <fontaine@cs.uni-duesseldorf.de>
2008-11-07 17:38:13 +0100

    towards removing all strings from the AST
    define Token datatype for csp-builtin-functions (Union, etc) that used to
    be represented as the concrete string in the AST
    
    todo : testing + token-datatype for Symbols
    
    darcs-hash:20081107163813-44086-da3ff4ecab9673265d582b82e65afa3eb174e8fc

---
+++ src/Language/CSPM/TokenClasses.hs
@@ -0,0 +1,89 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Language.CSPM.TokenClasses
+-- Copyright   :  (c) Fontaine 2008
+-- License     :  BSD
+-- 
+-- Maintainer  :  Fontaine@cs.uni-duesseldorf.de
+-- Stability   :  provisional
+-- Portability :  GHC-only
+--
+-- This module contains the datatype Tokens.
+
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving #-}
+
+module Language.CSPM.TokenClasses
+where
+
+import Data.Typeable (Typeable)
+import Data.Generics.Basics (Data)
+import Data.Generics.Instances ()
+
+data TokenClass
+  = LInteger
+  | LString
+  | LCspKeyword Keyword
+  | LCspBuiltIn BuiltIn     -- builtIns called by name
+  | LCspId
+  | LCspsym
+  | LIdent
+  | LCSPFDR      -- needed for special assertions
+  | LLComment
+  | LBComment
+  | LEOF
+  | LInclude
+--  deriving (Show,Eq,Ord,Enum,Ix, Typeable, Data)
+  deriving (Show,Eq,Ord, Typeable, Data)
+
+--allTokenClasses :: [TokenClass]
+--allTokenClasses = [LInteger .. LInclude]
+
+-- csp-specific
+data Keyword
+  = T_channel
+  | T_datatype
+  | T_nametype
+  | T_subtype
+  | T_assert
+  | T_pragma
+  | T_transparent
+  | T_external
+  | T_print
+  | T_if
+  | T_then
+  | T_else
+  | T_let
+  | T_within
+  deriving (Show,Eq,Ord,Typeable, Data)
+
+-- builtin functions
+data BuiltIn
+  = T_true
+  | T_false
+  | T_not
+  | T_and
+  | T_or
+  | T_union
+  | T_inter
+  | T_diff
+  | T_Union
+  | T_Inter
+  | T_member
+  | T_card
+  | T_empty
+  | T_set
+  | T_Set
+  | T_Seq
+  | T_null
+  | T_head
+  | T_tail
+  | T_concat
+  | T_elem
+  | T_length
+  | T_STOP
+  | T_SKIP
+  | T_Events
+  | T_Int
+  | T_Bool
+  | T_CHAOS
+  deriving (Show,Eq,Ord,Typeable, Data)
