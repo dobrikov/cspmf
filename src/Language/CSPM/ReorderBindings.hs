87c15c48d2cfdbb96d2d66339a0526b1b732b2fd
Marc Fontaine <fontaine@cs.uni-duesseldorf.de>
2008-11-21 17:00:52 +0100

    work on ReorderBindings
    Ignore-this: 5d3646cf8b4d21789af2bd9135d44094
    
    darcs-hash:20081121160052-44086-24c3eb0abc1a52bd7dcca66ce5e4e8775d75fa47

--- src/Language/CSPM/ReorderBindings.hs
+++ src/Language/CSPM/ReorderBindings.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Language.CSPM.ReorderBindings
@@ -9,8 +10,9 @@
 -- Portability :  GHC-only
 --
 -- Rule out multiple recursive bindinggroups.
--- Reorder bindings for call by value strategy.
--- 
+-- Reorder bindings for a call by value strategy.
+-- ToDo: think if this is correct.
+-- ToDo: add support for channel delcs and other top-level decls
 
 module Language.CSPM.ReorderBindings
   (
@@ -23,9 +25,13 @@ import qualified Language.CSPM.AST as AST
 import qualified Language.CSPM.SrcLoc as SrcLoc
 
 import Data.Data
-import Data.Generics.Schemes (everywhereM)
-import Data.Generics.Aliases (mkT,extQ)
+import Data.Generics.Schemes (everything,everywhereM)
+import Data.Generics.Aliases (mkM,extQ,mkQ)
 import Data.Generics.Basics (gmapQ,toConstr,showConstr)
+import Control.Exception (Exception)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Control.Monad.Error
 
 data BindError
   = BindError
@@ -34,20 +40,48 @@ data BindError
   ,bindErrorLoc :: SrcLoc.SrcLoc
   } deriving (Show,Typeable)
 
-instance Exception RenameError
+instance Exception BindError
+instance Error BindError where
+  noMsg    = BindError "unknown" SrcLoc.NoLocation
+  strMsg x = BindError x SrcLoc.NoLocation
 
 type RM x = Either BindError x
 
-- | reorderModule expects an AST that has gone through renaming
+-- | reorderModule expects an AST that has gone through renaming
 reorderModule :: LModule -> RM LModule
-reorderModule ast = reorderRecursive ast >>= reorderTopLevel
+reorderModule ast = do
+  a2 <- everywhereM (mkM fixLet) ast
+  reorderTopLevel a2
   where
-    reorderRecursive :: LModule -> RM LModule
-    reorderRecursive = everywhereM (mkT fixLet) ast
     fixLet :: LExp -> RM LExp
-    fixLet node = case unlabel node of
+    fixLet node = case unLabel node of
        Let decls e -> do
           d <- reorderGroup decls
           return $ setNode node $ Let d e
-       _ -> return e 
+       _ -> return node
          
+    reorderTopLevel :: LModule -> RM LModule
+    reorderTopLevel = error "reorder Toplevel"
+
+reorderGroup :: [LDecl] -> RM [LDecl]
+reorderGroup l = do
+  let d = map (\x -> (x, defs x, uses x)) l
+  return $ error "reorderGroup"
+
+-- | Compute the set of Identifier that get defined 
+-- | Todo: unit-tests for this
+defs :: LDecl -> Set UniqueIdent
+defs d = case unLabel d of
+  PatBind lpat _ -> everything Set.union ( Set.empty `mkQ` fromPattern) lpat
+  FunBind i _ -> Set.singleton $ unUIdent $ unLabel i
+  _ -> Set.empty  -- todo : add missing cases for top-level decls
+  where
+    fromPattern :: Pattern -> Set UniqueIdent
+    fromPattern (VarPat i) = Set.singleton $ unUIdent $ unLabel i
+    fromPattern _ = Set.empty
+
+-- | Uses is NOT the free names analysis.
+-- | We omit everything inside prefix-operations.
+-- | Todo: think if this is correkt
+uses :: LDecl -> Set UniqueIdent
+uses = error "ueses"
