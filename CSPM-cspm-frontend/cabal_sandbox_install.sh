e51a48a64e6de6f91fb0c1da01a0b4475506035a
dobrikov <dobrikov@cs.uni-duesseldorf.de>
2015-07-16 11:33:56 +0200

    Added a hack for re-installing libraries on leopard64

--- CSPM-cspm-frontend/cabal_sandbox_install.sh
+++ CSPM-cspm-frontend/cabal_sandbox_install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+# a hack for (re-)installing hashable on MacOS X 10.6
+ForceInstallHashable() {
+	OS=$(uname)
+	echo "operation system: $OS"
+	if [ "$OS" = "Darwin" ]
+	then
+		#statements
+		version=$(sw_vers | grep '10.6')
+		if [ -n "$version" ]
+		then
+			#statements
+			echo "force reinstalling hashable"
+			cabal install --reinstall --force-reinstall hashable profunctors
+		fi
+	fi
+}
+
 # cabal sandbox delete                       # Built-in command
 rm -rf .cabal-sandbox cabal.sandbox.config # uninstalling sandbox
 
@@ -11,6 +29,9 @@ cabal sandbox add-source ../CSPM-ToProlog
 
 # install only dependencies
 cabal install --only-dependencies
+
+# re-intsall hashable and profunctors on MacOS X 10.6
+ForceInstallHashable
 
 #build cspmf-tool
 cabal build
